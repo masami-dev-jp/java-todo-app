a0aba4d5a06f05d4f0d99977a809e0149be57244
Masami <yossy-m3068@ezweb.ne.jp>
2026-05-01 20:09:04 +0900

    Taskクラスにコンストラクタを実装 READMEの文言を修正

--- src/Task.java
+++ src/Task.java
@@ -3,9 +3,17 @@ public class Task {
     String title;
     TaskStatus taskStatus;
 
+    // enum：ステータスで取得する値を限定するためこの3つのみを許可
     enum TaskStatus {
         NOT_STARTED,
         IN_PROGRESS,
         DONE
     }
+
+    // コンストラクタ：Taskインスタンス生成時の初期化処理（titleは引数で受け取り、taskStatusは未着手で固定）
+    public Task(String title) {
+        this.title = title;
+        this.taskStatus = TaskStatus.NOT_STARTED;
+    }
+
 }
