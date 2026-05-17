0db1543b25173aa6f074f91db7f390cfa989fe26
Masami <im.m.yoshizawa@gmail.com>
2026-05-18 02:37:25 +0900

    List import追加

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 //TIP コードを<b>実行</b>するには、<shortcut actionId="Run"/> を押すか
 // ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Task task1 = new Task("Todoアプリ作成");
