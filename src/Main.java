3413f9a48fb78cba159bc50f17323bf2d9ce320a
Masami <im.m.yoshizawa@gmail.com>
2026-05-19 03:44:17 +0900

    ArrayListを追加してTask複数管理とコメント追加

--- src/Main.java
+++ src/Main.java
@@ -5,23 +5,49 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+
+        // Taskインスタンス生成（タスク作成）
         Task task1 = new Task("Todoアプリ作成");
 
+        // setterを使用してTaskから作ったオブジェクトtask1にIDと進捗状態を設定
         task1.setId(1);
         task1.setTaskStatus(Task.TaskStatus.IN_PROGRESS);
 
+        // getterを使用してtask1のid,title,taskStatusの情報を表示
         System.out.println(task1.getId());
         System.out.println(task1.getTitle());
         System.out.println(task1.getTaskStatus());
 
+        // 2つ目のTaskインスタンス生成
         Task task2 = new Task("筋トレ");
 
+        // setterを使用してTaskから作ったオブジェクトtask2にIDと進捗状態を設定
         task2.setId(2);
         task2.setTaskStatus(Task.TaskStatus.IN_PROGRESS);
 
+        // getterを使用してtask2のid,title,taskStatusの情報を表示
         System.out.println(task2.getId());
         System.out.println(task2.getTitle());
         System.out.println(task2.getTaskStatus());
+
+
+        // Taskを複数管理するためのList(tasks)を作成
+        List<Task> tasks = new ArrayList<>();
+
+        // List(tasks)にtask1,task2を追加
+        tasks.add(task1);
+        tasks.add(task2);
+
+        // List(tasks)の0番目のid,title,taskStatusを取得して表示
+        System.out.println(tasks.get(0).getId());
+        System.out.println(tasks.get(0).getTitle());
+        System.out.println(tasks.get(0).getTaskStatus());
+
+        // List(tasks)の1番目のid,title,taskStatusを取得して表示
+        System.out.println(tasks.get(1).getId());
+        System.out.println(tasks.get(1).getTitle());
+        System.out.println(tasks.get(1).getTaskStatus());
+
 
     }
 
--- src/Task.java
+++ src/Task.java
@@ -1,7 +1,13 @@
 public class Task {
+    // タスクID
     private int id;
+
+    // タスク名
     private String title;
+
+    // タスク進捗状態
     private TaskStatus taskStatus;
+
 
     // enum：ステータスで取得する値を限定するためこの3つのみを許可
     enum TaskStatus {
@@ -16,26 +22,36 @@ public class Task {
         this.taskStatus = TaskStatus.NOT_STARTED;
     }
 
+
+    // setter：フィールドへ値を設定するため使用　カプセル化
+    // getter：フィールドの値を取得するため使用　カプセル化
+
+    // タスクIDに値をセット(setter)
     public void setId(int id) {
         this.id = id;
     }
 
+    // タスクIDの値を取得(getter)
     public int getId() {
         return id;
     }
 
+    // タスク名に値をセット(setter)
     public void setTitle(String title) {
         this.title = title;
     }
 
+    // タスク名の値を取得(getter)
     public String getTitle() {
         return title;
     }
 
+    // タスク進捗状態に値をセット(setter)
     public void setTaskStatus(TaskStatus taskStatus) {
         this.taskStatus = taskStatus;
     }
 
+    // タスク進捗状態の値を取得(getter)
     public TaskStatus getTaskStatus() {
         return taskStatus;
     }
