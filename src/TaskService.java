ccd1ce5345b820242d2f34c8764a83aed941ccaa
Masami <im.m.yoshizawa@gmail.com>
2026-05-23 05:38:18 +0900

    TaskServiceクラスにタスク追加処理(addTask)を実装

--- src/TaskService.java
+++ src/TaskService.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TaskService {
+
+    private int nextId = 1;
+
+    List<Task> tasks = new ArrayList<>();
+
+    public void addTask(String title) {
+        Task task = new Task(title);
+        task.setId(nextId);
+        tasks.add(task);
+
+        nextId++;
+    }
 }
