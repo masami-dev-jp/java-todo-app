f431ca505cb0f3579cd6eb59f57e7930cf1b5cd5
Masami <im.m.yoshizawa@gmail.com>
2026-05-31 20:04:44 +0900

    switch文を用いたCLIメニューを実装し、タスクの追加・一覧・ステータス更新まで対応

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
-//TIP コードを<b>実行</b>するには、<shortcut actionId="Run"/> を押すか
-// ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
-import java.util.ArrayList;
-import java.util.List;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,8 +7,78 @@ public class Main {
         // TaskServiceインスタンス生成しserviceに代入
         TaskService service = new TaskService();
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("1. 一覧表示");
+        System.out.println("2. タスク追加");
+        System.out.println("3. タスクステータス更新");
+        System.out.println("4. タスク削除");
+
+        int menu = scanner.nextInt();
+        scanner.nextLine();
+
+        Task.TaskStatus newStatus = null;
+
+        switch (menu) {
+            case 1:
+                System.out.println("タスク一覧を表示します");
+                service.listTasks();
+                break;
+
+            case 2:
+                System.out.println("追加するタスクを入力してください");
+                String title = scanner.nextLine();
+                service.addTask(title);
+                service.listTasks();
+                break;
+
+            case 3:
+                System.out.println("更新するタスクIDを入力してください");
+                int id = scanner.nextInt();
+                scanner.nextLine();
+
+                System.out.println("更新後のステータスを選択してください");
+                System.out.println("1. NOT_STARTED");
+                System.out.println("2. IN_PROGRESS");
+                System.out.println("3. DONE");
+
+                int statusMenu = scanner.nextInt();
+                scanner.nextLine();
+
+                switch (statusMenu) {
+                    case 1:
+                        newStatus = Task.TaskStatus.NOT_STARTED;
+                        break;
+
+                    case 2:
+                        newStatus = Task.TaskStatus.IN_PROGRESS;
+                        break;
+
+                    case 3:
+                        newStatus = Task.TaskStatus.DONE;
+                        break;
+
+                    default:
+                        throw new IllegalAccessException("不正な入力です");
+
+                }
+                service.updateTaskStatus(id,newStatus);
+                break;
+
+            case 4:
+                System.out.println("タスク削除");
+                break;
+
+            default:
+                System.out.println("存在しないメニューです");
+        }
+
+
+
+
+
         // serviceにタスク追加
-        service.addTask("Todoアプリ作成");
+/*        service.addTask("Todoアプリ作成");
         service.addTask("筋トレ");
 
         // タスクステータス更新
@@ -20,7 +88,7 @@ public class Main {
         service.deleteTask(1);
 
         // タスク一覧表示
-        service.listTasks();
+        service.listTasks();*/
 
     }
 }
